79180dfa68da538e0927b506f5d821115a538261
Harisson Nascimento <harisson.nas@gmail.com>
2021-02-18 09:18:55 -0300

    feat: initial commit

---
+++ src/main/java/br/com/harisson/desafiocasadocodigo/DesafioCasaDoCodigoApplication.java
@@ -0,0 +1,13 @@
+package br.com.harisson.desafiocasadocodigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioCasaDoCodigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioCasaDoCodigoApplication.class, args);
+	}
+
+}
